c531d55c046a332cf33e87d71c6b0fcdc6568c9d
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2021-09-26 22:03:06 +0300

    collect static

---
+++ staticfiles/home.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $("#botton-slide-in-blurred-tr").click(
+       function (){ $("#slide-in-blurred-tr").toggle(
+            function () {$("#slide-in-blurred-tr").addClass("slide-in-blurred-tr"),
+                            $("#slide-in-blurred-tr").removeClass("invisible")},
+        )}
+
+        )})
+
+$(document).ready(function () {
+    $("#uploadbotton-slide-in-blurred-tr").click(
+        function (){ $("#upload-slide-in-blurred-tr").toggle(
+            function () {$("#upload-slide-in-blurred-tr").addClass("slide-in-blurred-tr"),
+                            $("#upload-slide-in-blurred-tr").removeClass("invisible")},
+        )}
+
+        )})
+
+
