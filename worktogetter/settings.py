3e8794835a96aabeb85fdd24c74509ad6c243804
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2021-09-26 21:57:42 +0300

    modify settings.py

--- worktogetter/settings.py
+++ worktogetter/settings.py
@@ -130,10 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIR = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'root')
 ]
 
 MEDIA_URL = 'media/'
